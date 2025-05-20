c79b44b8e43a1f3122f92dac259131c092de8b62
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-05-20 14:33:54 +0100

    First commit to push all the files to the remote repository

---
+++ js/app-logistics-fleet.js
@@ -0,0 +1,73 @@
+(() => {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoidGhvbXBzb24yMDI1IiwiYSI6ImNtOG5ibmw1dzBhNm8yanF6aHpvNTA0ZjQifQ.h7Q6PaMPTxFZ60iNxEbHuQ";
+  let o = {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          properties: { iconSize: [20, 42], message: "1" },
+          geometry: { type: "Point", coordinates: [-73.999024, 40.75249842] },
+        },
+        {
+          type: "Feature",
+          properties: { iconSize: [20, 42], message: "2" },
+          geometry: { type: "Point", coordinates: [-74.03, 40.75699842] },
+        },
+        {
+          type: "Feature",
+          properties: { iconSize: [20, 42], message: "3" },
+          geometry: { type: "Point", coordinates: [-73.967524, 40.7599842] },
+        },
+        {
+          type: "Feature",
+          properties: { iconSize: [20, 42], message: "4" },
+          geometry: { type: "Point", coordinates: [-74.0325, 40.742992] },
+        },
+      ],
+    },
+    a = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/light-v9",
+      center: [6.5033234, 3.3564706],
+      zoom: 12.25,
+    });
+  for (let s of o.features) {
+    var e = document.createElement("div"),
+      i = s.properties.iconSize[0],
+      l = s.properties.iconSize[1];
+    (e.className = "marker"),
+      e.insertAdjacentHTML(
+        "afterbegin",
+        '<img src="' +
+          assetsPath +
+          '../images/fleet-car.png" alt="Fleet Car" width="20" class="rounded-3" id="carFleet-' +
+          s.properties.message +
+          '">'
+      ),
+      (e.style.width = i + "px"),
+      (e.style.height = l + "px"),
+      (e.style.cursor = "pointer"),
+      new mapboxgl.Marker(e).setLngLat(s.geometry.coordinates).addTo(a);
+    let t = document.getElementById("fl-" + s.properties.message),
+      r = document.getElementById("carFleet-" + s.properties.message);
+    t.addEventListener("click", function () {
+      var e = document.querySelector(".marker-focus");
+      Helpers._hasClass("active", t)
+        ? (a.flyTo({
+            center: o.features[s.properties.message - 1].geometry.coordinates,
+            zoom: 16,
+          }),
+          e && Helpers._removeClass("marker-focus", e),
+          Helpers._addClass("marker-focus", r))
+        : Helpers._removeClass("marker-focus", r);
+    });
+  }
+  var t = document.getElementById("carFleet-1");
+  Helpers._addClass("marker-focus", t),
+    document
+      .querySelector(".mapboxgl-control-container")
+      .classList.add("d-none"),
+    (t = $(".logistics-fleet-sidebar-body")).length &&
+      new PerfectScrollbar(t[0], { wheelPropagation: !1, suppressScrollX: !0 });
+})();
